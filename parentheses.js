d3499442da7052dfb562f51f3a2ad01408f5c371
alishaevn <alieva.inc@gmail.com>
2018-04-06 13:41:35 -0700

    parser function can push and pop the correct brackets

--- parentheses.js
+++ parentheses.js
@@ -7,18 +7,14 @@ A stack can be used to ensure that an arithmetic expression has balanced parenth
 
 For version 1, the parentheses you need to consider are ( and ). Finding a closed parenthesis without an open parenthesis is an error (report the location of the close); reaching the end of the string while still "holding" an open parenthesis is also an error (report the location of the open).
 
-Extension exercise: Recognize three pairs of brackets: (), [], and {}. These must be correctly nested; "([)]" is incorrect, and should report an error at the ), stating that you were expecting a ] but found a ). If this is starting to look and sound very familiar, congratulations - you're beginning to write a simple language parser!
-
 Extension extension exercise: Also recognize two types of quote character: "" and ''. Inside quotes, brackets aren't counted at all - in fact, nothing is counted until you reach the corresponding close quote.
 */
 
 const parentheses = parens => {
   // add to stack on '('
   // remove from stack on ')'
+
   const stack = new Stack();
-
-
-  console.log('STACK BEGIN:', stack);
 
   for (let i = 0; i < parens.length; i++) {
     if (parens[i] === '(') {
@@ -33,14 +29,57 @@ const parentheses = parens => {
       }
     }
   }
-  console.log('');
-  console.log('STACK END:', stack);
 
   return stack.top ? `There is an additional '${stack.top.data.character}' at index ${stack.top.data.index}` : 'success';
 
 };
 
 // console.log('equal:', JSON.stringify(parentheses('(3+5)'))); // version 1, equal parens
-console.log('**(:', JSON.stringify(parentheses('(()'))); // version 1, 1 more open parens
-console.log('');
-console.log('**):', JSON.stringify(parentheses('())'))); // version 1, 1 more closed parens
+// console.log('**(:', JSON.stringify(parentheses('(()'))); // version 1, 1 more open parens
+// console.log('**):', JSON.stringify(parentheses('())'))); // version 1, 1 more closed parens
+
+
+
+
+
+/* =============================
+  EXTENSION EXERCISE
+
+Extension exercise: Recognize three pairs of brackets: (), [], and {}. These must be correctly nested; "([)]" is incorrect, and should report an error at the ), stating that you were expecting a ] but found a ). If this is starting to look and sound very familiar, congratulations - you're beginning to write a simple language parser!
+============================= */
+
+const parser = str => {
+  // if str[i] is a brackets.key, add to the stack
+  // if str[i] is a brackets.value remove from the stack IF stack.top equals corresponding bracket.key
+  // if stack.top is null OR another bracket.key, throw an error
+  // if you get to the end && the stack is empty, success!
+
+  const stack = new Stack();
+
+  let brackets = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  };
+
+  const keys = Object.keys(brackets); // keys: [ '(', '[', '{' ]
+  let values = Object.values(brackets); // values: [ ')', ']', '}' ]
+  
+  for (let i = 0; i < str.length; i++) {
+    if (keys.includes(str[i])) {
+      stack.push(str[i]);
+    }
+    else if (values.includes(str[i])) {
+      stack.pop();
+    }
+    // if the i is in values, but stack.top is NOT it's match... do this
+  }
+
+  return stack;
+
+};
+
+console.log('GOOD:', JSON.stringify(parser('()[]{}')));
+
+// console.log('GOOD:', JSON.stringify(parser('()[]{}')));
+// console.log('BAD:', JSON.stringify(parser('([}{)]')));
