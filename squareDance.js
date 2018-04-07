81c74ff814b6c936f795a6254e8c939d55a3a1f3
alishaevn <alieva.inc@gmail.com>
2018-04-07 14:33:40 -0700

    deleted all the console logs

--- squareDance.js
+++ squareDance.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Queue = require('./Queue');
-const { display } = require('./supplementalQFunctions');
 
 /*
 As people come to the dance floor, they should be paired off as quickly as possible: man with woman, man with woman, all the way down the line. If several men arrive in a row, they should be paired in the order they came, and likewise if several women do. Maintain a queue of "spares" (men for whom you have no women yet, or vice versa), and pair them as appropriate.
@@ -34,34 +33,22 @@ const squareDance = queue => {
   // if the two genders != and spares head is the same gender, add the spare to pairs and the dancer to spares
   
   while (dancers.head !== null) {
-    console.log(dancers.head.value.gender);
     
     if (pairs.tail.value.gender === dancers.head.value.gender) {
       spares.enqueue(dancers.dequeue());
-      console.log('spares');
     }
     else if ((pairs.tail.value.gender !== dancers.head.value.gender) && !spares.head) {
       pairs.enqueue(dancers.dequeue());
-      console.log('pair dancer 1');
     }
     else if ((pairs.tail.value.gender !== dancers.head.value.gender) && (spares.head.value.gender !== dancers.head.value.gender)) {
       pairs.enqueue(dancers.dequeue());
-      console.log('pair dancer 2');
     }
     else if ((pairs.tail.value.gender !== dancers.head.value.gender) && (spares.head.value.gender === dancers.head.value.gender)) {
       pairs.enqueue(spares.dequeue());
       spares.enqueue(dancers.dequeue());
-      console.log('spare to pair');
     }
   }
 
-  console.log('PAIRS:', display(pairs));
-  console.log('');
-  console.log('SPARES:', display(spares));
-  console.log('');
-  console.log('DANCERS:', display(dancers));
-
-  
   let inLine = '';
   while (spares.head) {
     if (!spares.head.next) {
@@ -81,9 +68,6 @@ const squareDance = queue => {
     }
     pairs.dequeue();
   }
-
-  console.log(inLine);
-  console.log(onTheFloor);
 
   return `${onTheFloor} are on the floor dancing while ${inLine} are still waiting for a pair!`;
 
